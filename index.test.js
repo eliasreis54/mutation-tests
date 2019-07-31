06425b3a2d350ae5cdd626fcf426e4efe5f2748c
Elias Reis <eliasreis54@gmail.com>
2019-07-30 22:42:49 -0300

    all mutant killed

--- index.test.js
+++ index.test.js
@@ -3,5 +3,7 @@ const oldEnough = require('./index');
 describe('Test my function', () => {
   test('Old enough', () => {
     expect(oldEnough(19)).toBe(true);
+    expect(oldEnough(17)).toBe(false);
+    expect(oldEnough(18)).toBe(true);
   });
 });
